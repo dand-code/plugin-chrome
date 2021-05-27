fd716bbe2aa27cb9721fc9f344f5db22fa82cd18
Dandara Almeida <dandara.avazquez@gmail.com>
2021-05-27 16:58:33 +0200

    include basic style

--- src/hooks/selector.js
+++ src/hooks/selector.js
@@ -2,14 +2,15 @@
 import { selector } from 'recoil';
 import { wordListState } from '../hooks/atom';
 
+
 export const updateWordList = selector({
     key: 'updateWordList',
     get: ({get}) => {
         const listItem = get(wordListState);
         console.log(listItem);
         return listItem.map((word, index) => (
-            <li key={index + 100}>
-                {word}
+            <li key={index + 100}>     
+                <p>{word}</p>
             </li>
         ));   
     },
