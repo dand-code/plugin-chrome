22cf640a24e047188e74b7bc8f50a16e36a44982
Dandara Almeida <dandara.avazquez@gmail.com>
2021-05-27 13:09:59 +0200

    delete recoil persist

--- src/hooks/atom.js
+++ src/hooks/atom.js
@@ -1,7 +1,4 @@
 import { atom } from 'recoil';
-import { recoilPersist } from 'recoil-persist'
-
-const { persistAtom } = recoilPersist()
 
 export const textState = atom({
     key: 'text', 
@@ -10,6 +7,5 @@ export const textState = atom({
 export const wordListState = atom({
     key: 'wordListState', 
     default: [], 
-    effects_UNSTABLE: [persistAtom]
 });
 
--- src/hooks/selector.js
+++ src/hooks/selector.js
@@ -1,3 +1,4 @@
+
 import { selector } from 'recoil';
 import { wordListState } from '../hooks/atom';
 
@@ -5,6 +6,7 @@ export const updateWordList = selector({
     key: 'updateWordList',
     get: ({get}) => {
         const listItem = get(wordListState);
+        console.log(listItem);
         return listItem.map((todo, index) => (
             <li key={index + 100}>
                 {todo}
