7f2a4e9544bbb95184869c4f3bc3a553618349a0
Dandara Almeida <dandara.avazquez@gmail.com>
2021-06-03 11:09:00 +0200

    add show new eord on screen test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,15 +3,17 @@ import App from '../App';
 import { RecoilRoot } from 'recoil';
 
 describe('Initialize App component', () => {
-    it('should see the title, form inputs and "add" button', () => {
-        render(<RecoilRoot><App /></RecoilRoot>);
-        const title = screen.getByRole("heading", { name: /words/i });
-        const word = screen.getByLabelText(/word/i);
-        const note = screen.getByLabelText(/note/i);
+    describe("The fisrt time to open the app", () => {
+        it('should see the title, form inputs and "add" button', () => {
+            render(<RecoilRoot><App /></RecoilRoot>);
+            const title = screen.getByRole("heading", { name: /words/i });
+            const word = screen.getByLabelText(/word/i);
+            const note = screen.getByLabelText(/note/i);
 
 
-        expect(title).toBeInTheDocument();
-        expect(word).toBeInTheDocument();
-        expect(note).toBeInTheDocument();
-    })
-})
+            expect(title).toBeInTheDocument();
+            expect(word).toBeInTheDocument();
+            expect(note).toBeInTheDocument();
+        });
+    });
+});
